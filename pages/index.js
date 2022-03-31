1f4811fe7c3130efba89160d40d539b2e5b4973d
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-03-31 12:50:09 +0200

    infocontainer global stats crypto

--- pages/index.js
+++ pages/index.js
@@ -4,29 +4,30 @@ import Image from "next/image";
 import { useEffect } from "react";
 import styles from "../styles/Home.module.css";
 
-export default function Home({ data }) {
-  console.log(data);
+export default function Home() {
   return (
     <div className={styles.container}>
-      <div>
-        <h1>All Cryptocurrencies</h1>
+      <div className={styles.title}>
+        <h1>Global Crypto Stats</h1>
+      </div>
+      <div className={styles.globalContainer}>
+        <div className={styles.infoContainer}>
+          <p>Total Coins</p>
+          <h5>42,222</h5>
+        </div>
+        <div className={styles.infoContainer}>
+          <p>Total Exchanges</p>
+          <h5>373</h5>
+        </div>
+        <div className={styles.infoContainer}>
+          <p>Total Markets</p>
+          <h5>2.3T</h5>
+        </div>
+        <div className={styles.infoContainer}>
+          <p>Total Market Cap</p>
+          <h5>44.44k</h5>
+        </div>
       </div>
     </div>
   );
-}
-
-export async function getStaticProps(context) {
-  try {
-    const result = await axios.get(
-      "https://jsonplaceholder.typicode.com/todos/1"
-    );
-    const data = result.data;
-    return {
-      props: {
-        data: data,
-      },
-    };
-  } catch (error) {
-    return { notFound: true };
-  }
 }
