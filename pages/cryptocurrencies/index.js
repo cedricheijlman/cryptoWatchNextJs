3d5f3c4faf54b473149c9d76ce998f3d4c1e7f44
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-04-01 15:42:36 +0200

    all cryptos fetch

--- pages/cryptocurrencies/index.js
+++ pages/cryptocurrencies/index.js
@@ -1,11 +1,31 @@
 import React from "react";
+import CryptoList from "../../components/homepage/cryptoList";
+import styles from "./CryptocurrenciesPage.module.css";
 
-const CryptocurrenciesPage = () => {
+const CryptocurrenciesPage = ({ coinsInfo }) => {
+  console.log(coinsInfo);
   return (
-    <div>
+    <div className={styles.container}>
       <h1>CryptoCurrencies</h1>
+      <CryptoList items={coinsInfo.data} />
     </div>
   );
 };
 
 export default CryptocurrenciesPage;
+export async function getServerSideProps(context) {
+  const res = await fetch(`https://api.coinranking.com/v2/coins`, {
+    method: "GET",
+    headers: {
+      "x-access-token": process.env.API_KEY,
+    },
+  });
+
+  const coinsInfo = await res.json();
+
+  return {
+    props: {
+      coinsInfo,
+    },
+  };
+}
