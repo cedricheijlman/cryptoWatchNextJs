547be1000723fc9cb7c42771cbe4948f7d585135
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-04-01 17:02:02 +0200

    styling

--- pages/cryptocurrencies/[uid].js
+++ pages/cryptocurrencies/[uid].js
@@ -4,8 +4,8 @@ import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlin
 import TagIcon from "@mui/icons-material/Tag";
 import EmojiEventsOutlinedIcon from "@mui/icons-material/EmojiEventsOutlined";
 import BoltIcon from "@mui/icons-material/Bolt";
+import millify from "millify";
 const cryptoInfoPage = ({ id, coinInfo }) => {
-  console.log(coinInfo);
   return (
     <div className={styles.container}>
       <h2>
@@ -16,7 +16,7 @@ const cryptoInfoPage = ({ id, coinInfo }) => {
         cap and supply
       </p>
       <div className={styles.valueStatistics}>
-        <h2>{coinInfo.name} Value Satistics</h2>
+        <h2>{coinInfo.name} Value Statistics</h2>
         <p className={styles.valueStatistics__title}>
           An overview showing the stats of {coinInfo.name}
         </p>
@@ -25,35 +25,35 @@ const cryptoInfoPage = ({ id, coinInfo }) => {
             <MonetizationOnOutlinedIcon />
             <h5>Price to USD</h5>
           </div>
-          <p>${"46.7K"}</p>
+          <p>${millify(coinInfo.price)}</p>
         </div>
         <div className={styles.valueInfo}>
           <div className={styles.itemLeft}>
             <TagIcon />
             <h5>Rank</h5>
           </div>
-          <p>{"1"}</p>
+          <p>{coinInfo.rank}</p>
         </div>
         <div className={styles.valueInfo}>
           <div className={styles.itemLeft}>
             <BoltIcon />
             <h5>24H Volume</h5>
           </div>
-          <p>{"1"}</p>
+          <p>{millify(coinInfo["24hVolume"])}</p>
         </div>
         <div className={styles.valueInfo}>
           <div className={styles.itemLeft}>
             <MonetizationOnOutlinedIcon />
             <h5>Market Cap</h5>
           </div>
-          <p>{"1"}</p>
+          <p>{millify(coinInfo.marketCap)}</p>
         </div>
         <div className={styles.valueInfo}>
           <div className={styles.itemLeft}>
             <EmojiEventsOutlinedIcon />
-            <h5>Daily Average</h5>
+            <h5>All Time High</h5>
           </div>
-          <p>{"1"}</p>
+          <p>{millify(coinInfo.allTimeHigh.price)}</p>
         </div>
       </div>
     </div>
